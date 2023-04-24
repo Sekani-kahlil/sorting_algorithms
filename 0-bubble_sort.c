1fec925d6c5ec01b9fca160d4879564621bdc933
Sekani-Kahlil <Judef06@gmail.com>
2023-04-24 01:56:09 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,25 +11,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int tmp, sorted;
-
-	if (!array || size == 0)
-		return;
-
-	do {
-		sorted = 1;
-		for (i = 0; i < size - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				sorted = 0;
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				print_array(array, size);
-			}
-		}
-
-	} while (sorted == 0);
+	for (size_t i = 0; i < size - 1; i++)
+	{
+	for (size_t j = 0; j < size - i - 1; j++)
+	{
+	if (array[j] > array[j + 1])
+	{
+	int temp = array[j];
+	array[j] = array[j + 1];
+	array[j + 1] = temp;
+	}
+	}
+	// print array after each pass
+	printf("After pass %ld: ", i+1);
+	for (size_t k = 0; k < size; k++)
+	{
+	printf("%d ", array[k]);
+	}
+	printf("\n");
+	}
 }
